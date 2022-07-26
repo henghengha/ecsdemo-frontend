d81f68c314f59d46097185da09ee01d30f5fd8e5
henghengha <837526418@qq.com>
2022-07-26 11:01:58 +0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
   # Example endpoint that calls the backend nodejs api
   def index
     begin
-      req = Net::HTTP::Get.new(nodejs_uri.to_s)
+      #req = Net::HTTP::Get.new(nodejs_uri.to_s)
+      req = Net::HTTP::Get.new(nodejs_uri.path)
       res = Net::HTTP.start(nodejs_uri.host, nodejs_uri.port, :use_ssl => nodejs_uri.scheme == 'https') {|http|
         http.read_timeout = 2
         http.open_timeout = 2
@@ -29,7 +30,8 @@ class ApplicationController < ActionController::Base
     end
 
     begin
-      crystalreq = Net::HTTP::Get.new(crystal_uri.to_s)
+      #crystalreq = Net::HTTP::Get.new(crystal_uri.to_s)
+      crystalreq = Net::HTTP::Get.new(crystal_uri.path)
       crystalres = Net::HTTP.start(crystal_uri.host, crystal_uri.port, :use_ssl => crystal_uri.scheme == 'https') {|http|
         http.read_timeout = 2
         http.open_timeout = 2
